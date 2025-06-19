42fa3f3140113823f4047a2f04389015fadcfec4
Aaditya Sigdel <adityasigdel8@gmail.com>
2025-06-19 19:56:53 +0545

    context

--- src/GuessNum/Guess.jsx
+++ src/GuessNum/Guess.jsx
@@ -1,7 +1,12 @@
 export default function Guess(){
+    function HandleGuess(){
+        
+    }
     return(
         <>
         <h1>Welcome to guess the number</h1>
+        <input type="text" placeholder="Guess the number"/>
+        <button onClick={HandleGuess}>Guess</button>
         </>
     )
 }
